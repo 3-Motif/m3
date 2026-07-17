b89437cc6baf9acbae7343f8b7215afd45c91981
Jeigsaw <sumanull@outlook.com>
2026-07-17 20:02:45 +0545

    add ui elements

---
+++ main.js
@@ -0,0 +1,11 @@
+const fileUploadDialog = document.getElementById("fileUploadDialog");
+const fileInfoDialog = document.getElementById("fileInfoDialog");
+const spinner = document.getElementById("loader");
+const countSubgraphBtn = document.getElementById("countSubgraphBtn");
+const genRandomBtn = document.getElementById("genRandomBtn");
+const subgraphWindow = document.getElementById("subgraphWindow");
+
+// fileUploadDialog.style.display = "none";
+fileInfoDialog.style.display = "none";
+subgraphWindow.style.display = "none";
+spinner.style.display = "none";
